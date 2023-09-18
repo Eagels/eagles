a83a56bd7ed8c54b670188c783eebaa79a501298
gaoxiaochao <747750441@qq.com>
2023-09-18 10:05:30 +0800

    fix: 还原

--- assets/React-6aa57e00.js
+++
@@ -1,1 +0,0 @@
-import{d as e,c as a,a as t}from"./index-ab1a0c1a.js";const s=e({__name:"React",setup(n){return(c,o)=>(t(),a("span",null," React "))}});export{s as default};
---
+++ assets/Vue-197b3b76.js
@@ -0,0 +1,1 @@
+import{d as e,c as n,a}from"./index-f4bfe65e.js";const s=e({__name:"Vue",setup(t){return(o,c)=>(a(),n("span",null," Vue "))}});export{s as default};
